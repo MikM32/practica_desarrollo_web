e304d41257fbce877303e6c32ed75d829fff79fa
MikM32 <miguelmatute2004@gmail.com>
2026-08-13 17:49:42 -0400

    agregado: estado del ascensor (piso destino y estado)

--- js/ascensor.js
+++ js/ascensor.js
@@ -2,32 +2,46 @@
   "use strict";
 
   var pisoActual = 1;
+  var pisoDestino = null;
   var direccion = "detenido";
+  var estado = "detenido";
   var enMovimiento = false;
 
-  function subir(pisoDestino) {
+  function subir(piso) {
+    pisoDestino = piso;
     enMovimiento = true;
     direccion = "subiendo";
+    estado = "subiendo";
     var intervalo = setInterval(function () {
       pisoActual += 1;
       console.log("Piso actual: " + pisoActual);
       if (pisoActual >= pisoDestino) {
         clearInterval(intervalo);
+        estado = "atendiendo";
+        console.log("Solicitud atendida en el piso " + pisoActual);
         direccion = "detenido";
+        estado = "detenido";
+        pisoDestino = null;
         enMovimiento = false;
       }
     }, 1000);
   }
 
-  function bajar(pisoDestino) {
+  function bajar(piso) {
+    pisoDestino = piso;
     enMovimiento = true;
     direccion = "bajando";
+    estado = "bajando";
     var intervalo = setInterval(function () {
       pisoActual -= 1;
       console.log("Piso actual: " + pisoActual);
       if (pisoActual <= pisoDestino) {
         clearInterval(intervalo);
+        estado = "atendiendo";
+        console.log("Solicitud atendida en el piso " + pisoActual);
         direccion = "detenido";
+        estado = "detenido";
+        pisoDestino = null;
         enMovimiento = false;
       }
     }, 1000);
@@ -48,8 +62,14 @@
     getPisoActual: function () {
       return pisoActual;
     },
+    getPisoDestino: function () {
+      return pisoDestino;
+    },
     getDireccion: function () {
       return direccion;
+    },
+    getEstado: function () {
+      return estado;
     }
   };
 })();
